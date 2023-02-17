381250508ac4b6bf793e60b1bc2e4031484b945c
The Magppi <skiderdun@gmail.com>
2023-02-16 19:28:00 -0600

    first commit

---
+++ window.py
@@ -0,0 +1,15 @@
+## create window object
+from tkinter import *
+import tkinter as tk
+
+## create window object
+root = tk.Tk()
+
+## modify root window
+root.title("Hello, World!")
+root.geometry("200x100")
+
+## kick off the event loop
+root.mainloop()
+
+#!/usr/bin/env python3

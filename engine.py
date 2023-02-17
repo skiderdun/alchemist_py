6b4ff78b69658f9e05dc0f0003aaf1c7b4933d1f
Thomas Weiss <skiderdun@gmail.com>
2023-02-17 16:06:16 -0600

    update engine.py

--- engine.py
+++ engine.py
@@ -1,6 +1,7 @@
 
 import esper
 import tkinter as tk
+import keyboard as kb
 
 class Engine(tk.Tk):
     def __init__(self, **kwargs):
@@ -11,15 +12,50 @@ class Engine(tk.Tk):
         self.canvas.pack()
         self.name = "world"
         self.player = None
+        self.title("Alchemist: " + self.name)
+        self.x_pos = 0
+        self.y_pos = 0
+
+        # Kewarg processing
         if "world" in kwargs:
             self.name = kwargs["world"]
         elif "player" in kwargs:
             self.player = kwargs["player"]
 
-        
-        self.title("Alchemist: " + self.name)
+        ## add test sprite
+        self.sprite = tk.PhotoImage(file="assets/Tiles/LIL_DUDE.png")
 
-    def mainloop(self):
+        # add processors
+        self.world.add_processor(self.Position(self.x_pos, self.y_pos, self.canvas, self.canvas.create_image(self.x_pos, self.y_pos, image=self.sprite)))
+
+    def update(self):
         self.world.process()
-        super().mainloop()
-
+        self.title("Alchemist: " + self.name)
+        self.canvas.update()
+        self.canvas.delete("all")
+        
+    def mainloop(self):
+        while True:
+            self.update()
+    
+    class Position(esper.Processor):
+        def __init__(self, x_pos, y_pos, canvas, sprite):
+            self.x_pos = x_pos
+            self.y_pos = y_pos
+            self.canvas = canvas
+            self.sprite = sprite
+            
+        def process(self):    
+            right = kb.is_pressed("a")
+            left = kb.is_pressed("d")
+            up = kb.is_pressed("w")
+            down = kb.is_pressed("s")
+            if right:
+                self.x_pos += 1
+            elif left:
+                self.x_pos -= 1
+            elif up:
+                self.y_pos -= 1
+            elif down:
+                self.y_pos += 1
+            

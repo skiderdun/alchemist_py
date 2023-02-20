8ff1569f58c80138fe3af89ee9abc276158d3055
The Magppi <skiderdun@gmail.com>
2023-02-20 06:01:11 -0600

    complete rewrite of the game engine with piglet

--- debug.py
+++
@@ -1,7 +0,0 @@
-## function to check type of input
-## takes a type and a value and throws an error if the type is not correct
-def check_type(type, value):
-    if type(value) != type:
-        raise TypeError("Expected type " + str(type) + " but got " + str(type(value))) 
-    else:
-        return True
--- main.py
+++ main.py
@@ -1,14 +1,34 @@
 ## Alchemist: a 2D game with a sophisticated chemistry engine
 
-import debug
-import tkinter as tk
-from engine import Engine
+## import modules
+import os
+import pyglet
+from pyglet import app
+from pyglet.window import Window
 
 ## create main window
 
 def main():
-    world = Engine()
-    world.mainloop()
+    window = Window(1280, 780, caption="Alchemist")
+    batch = pyglet.graphics.Batch()
+
+    lil_dude = pyglet.resource.image("assets/player_sprites/LIL_DUDE.png")
+    lil_dude.anchor_x = lil_dude.width // 2
+    lil_dude.anchor_y = lil_dude.height // 2
+
+    walk = []
+    for spr in os.listdir("assets/player_sprites/walk"):
+        if spr.endswith(".png"):
+            walk.append(pyglet.resource.image("assets/player_sprites/walk/" + spr))
+    walk_animation = pyglet.image.Animation.from_image_sequence(walk, 0.1, True)
+    sprite = pyglet.sprite.Sprite(walk_animation, x=500, y=500, batch=batch)
+
+    @window.event
+    def on_draw():
+        window.clear();
+        batch.draw();
+    app.run()
+
 
 if __name__ == "__main__":
     main()
